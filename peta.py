9a29e307e5a398686f618c660f543eda6075396e
Haikal Ferdian <100331750+haikalfers@users.noreply.github.com>
2024-05-02 12:50:13 +0700

    Create peta.py

---
+++ peta.py
@@ -0,0 +1,12 @@
+graf = {
+    'Bekasi' : ('Karawang', 'Depok', 'Bogor'),
+    'Karawang' : ('Bekasi', 'Subang', 'Purwakarta', 'Cianjur'),
+    'Subang' : ('Karawang', 'Purwakarta', 'Indramayu', 'Sumedang', 'Bandung'),
+    'Indramayu' : ('Subang', 'Sumedang', 'Majalengka', 'Cirebon'),
+    'Cirebon' : ('Indramayu', 'Majalengka', 'Kuningan'),
+    'Kuningan' : ('Cirebon', 'Majalengka', 'Ciamis'),
+    'Ciamis' : ('Kuningan', 'Majalengka', 'Tasikmalaya'),
+    'Tasikmalaya' : ('Ciamis', 'Garut', 'Majalengka'),
+    'Garut' : ('Tasikmalaya', 'Sumedang', 'Bandung'),
+    'Bandung' : ('Purwakarta', 'Subang', "Sumedang", 'Garut'),
+}
